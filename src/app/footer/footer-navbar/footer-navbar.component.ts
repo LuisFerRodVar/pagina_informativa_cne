d3832ee911fb9e37362877506d4b5f109dc3b3a1
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-04-29 16:28:35 -0600

    Agregado el navbar del footer

--- src/app/footer/footer-navbar/footer-navbar.component.ts
+++ src/app/footer/footer-navbar/footer-navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './footer-navbar.component.html',
   styleUrl: './footer-navbar.component.css'
 })
