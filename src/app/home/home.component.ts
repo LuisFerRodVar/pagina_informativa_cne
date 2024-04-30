2d463bcc6064853e6b9ece26caf44fc46fa30738
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-04-29 22:27:53 -0600

    Add home header component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
