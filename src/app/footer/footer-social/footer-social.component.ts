33cade906b6586f399c9c1eee65dc91173b163f1
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-05-01 09:29:50 -0600

    Added fontawesome library and improved footer

--- src/app/footer/footer-social/footer-social.component.ts
+++ src/app/footer/footer-social/footer-social.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram, faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer-social',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer-social.component.html',
   styleUrl: './footer-social.component.css'
 })
 export class FooterSocialComponent {
-
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faYoutube = faYoutube;
+  faWhatsapp = faWhatsapp;
 }
