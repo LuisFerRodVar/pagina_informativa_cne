deb365e1fed1d6ce7fafd544cc0e9734451ec79b
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-05-07 03:17:27 -0600

    Added FAQ Accordion functionality

--- src/app/home/home-faq/home-faq.component.ts
+++ src/app/home/home-faq/home-faq.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
-
-
+import {
+  faPlus,
+  faMinus,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 interface FAQ {
   question: string;
@@ -15,9 +17,44 @@ interface FAQ {
   standalone: true,
   imports: [FontAwesomeModule],
   templateUrl: './home-faq.component.html',
-  styleUrl: './home-faq.component.css'
+  styleUrl: './home-faq.component.css',
 })
 export class HomeFaqComponent {
-  faPlus = faPlus
-  faMinus = faMinus
+  faPlus = faPlus;
+  faMinus = faMinus;
+
+  faIcon1: IconDefinition = faPlus;
+  faIcon2: IconDefinition = faPlus;
+  activeQuestionId: string | null = null;
+
+  toggleItem(id: string) {
+    const element = document.getElementById(id);
+    if (element) {
+      const isOpen = element.classList.contains('open');
+      const questions = document.querySelectorAll('.question');
+
+      questions.forEach((question: Element) => {
+        question.classList.remove('open');
+        const closeButton = question.querySelector('.close');
+        if (closeButton) {
+          closeButton.classList.remove('active');
+        }
+      });
+
+      if (!isOpen) {
+        element.classList.add('open');
+        this.activeQuestionId = id;
+        const closeButton = element.querySelector('.close');
+        if (closeButton) {
+          closeButton.classList.add('active');
+        }
+      } else {
+        this.activeQuestionId = null;
+      }
+    }
+  }
+
+  isQuestionActive(id: string): boolean {
+    return this.activeQuestionId === id;
+  }
 }
