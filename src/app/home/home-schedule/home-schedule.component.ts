0b6ae62d56cb255b55ce405e17125e5901fd9464
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-05-07 00:24:51 -0600

    Added news basic  component.

---
+++ src/app/home/home-schedule/home-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-schedule.component.html',
+  styleUrl: './home-schedule.component.css'
+})
+export class HomeScheduleComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,12 +4,14 @@ import { HomeAboutmeComponent } from './home-aboutme/home-aboutme.component';
 import { HomeActionsComponent } from './home-actions/home-actions.component';
 import { HomeInfoComponent } from './home-info/home-info.component';
 import { HomeNewComponent } from './home-new/home-new.component';
+import { HomeFaqComponent } from './home-faq/home-faq.component';
+import { HomeScheduleComponent } from './home-schedule/home-schedule.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeHeaderComponent, HomeAboutmeComponent, HomeActionsComponent, HomeActionsComponent, HomeInfoComponent, HomeNewComponent],
+  imports: [HomeHeaderComponent, HomeAboutmeComponent, HomeActionsComponent, HomeActionsComponent, HomeInfoComponent, HomeNewComponent, HomeFaqComponent, HomeScheduleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
