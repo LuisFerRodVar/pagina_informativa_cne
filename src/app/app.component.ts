ceddbfc4bf4d367645dc0c7f202009e174bc479d
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-05-15 08:27:01 -0600

    added responsive burguer menu icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuMobileComponent } from './menu-mobile/menu-mobile.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, MenuMobileComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pagina_informativa_cne';
+  faBars = faBars;
+  faX = faX;
+  open = false;
+  toggleOpen(){
+    this.open = !this.open;
+  }
 }
