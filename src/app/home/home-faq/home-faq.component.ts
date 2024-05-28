96d17cbd9ba00d59349defe22b3fc12d7795219d
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-05-28 00:33:50 -0600

    Refactoring FAQ seccion and adding responsive

--- src/app/home/home-faq/home-faq.component.ts
+++ src/app/home/home-faq/home-faq.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPlus, faMinus, IconDefinition  } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlus,
+  faMinus,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 interface FAQ {
+  id: number;
   question: string;
   answer: string;
-  active: boolean;
 }
 
 @Component({
@@ -13,70 +17,43 @@ interface FAQ {
   standalone: true,
   imports: [FontAwesomeModule],
   templateUrl: './home-faq.component.html',
-  styleUrl: './home-faq.component.css',
+  styleUrls: ['./home-faq.component.css'],
 })
 export class HomeFaqComponent {
   faPlus = faPlus;
   faMinus = faMinus;
 
-  faIcon1: IconDefinition = faPlus;
-  faIcon2: IconDefinition = faPlus;
-  faIcon3: IconDefinition = faPlus;
-  faIcon4: IconDefinition = faPlus;
+  openFaqId: number | null = null;
 
-  toggleItem(id: string) {
-    const element = document.getElementById(id);
-    
-    if (element) {
-      const isOpen = element.classList.contains('open');
-      const questions = document.querySelectorAll('.question');
-
-      questions.forEach((question: Element) => {
-        question.classList.remove('open');
-        const closeButton = question.querySelector('.close');
-        if (closeButton) {
-          closeButton.classList.remove('active');
-        }
-      });
-
-      if (!isOpen) {
-        element.classList.add('open');
-        const closeButton = element.querySelector('.close');
-        if (closeButton) {
-          closeButton.classList.add('active');
-        }
-        if (id === 'faq1') {
-          this.faIcon1 = this.faMinus;
-          this.faIcon2 = this.faPlus;
-          this.faIcon3 = this.faPlus;
-          this.faIcon4 = this.faPlus;
-        } else if (id === 'faq2') {
-          this.faIcon1 = this.faPlus;
-          this.faIcon2 = this.faMinus;
-          this.faIcon3 = this.faPlus;
-          this.faIcon4 = this.faPlus;
-        } else if (id === 'faq3') {
-          this.faIcon1 = this.faPlus;
-          this.faIcon2 = this.faPlus;
-          this.faIcon3 = this.faMinus;
-          this.faIcon4 = this.faPlus;
-        } else if (id === 'faq4') {
-          this.faIcon1 = this.faPlus;
-          this.faIcon2 = this.faPlus;
-          this.faIcon3 = this.faPlus;
-          this.faIcon4 = this.faMinus;
-        }
-      } else {
-        if (id === 'faq1') {
-          this.faIcon1 = this.faPlus;
-        } else if (id === 'faq2') {
-          this.faIcon2 = this.faPlus;
-        } else if (id === 'faq3') {
-          this.faIcon3 = this.faPlus;
-        } else if (id === 'faq4') {
-          this.faIcon4 = this.faPlus;
-        }
-      }
-    }
+  toggleFaq(id: number) {
+    this.openFaqId = this.openFaqId === id ? null : id;
   }
+
+  faqs: FAQ[] = [
+    {
+      id: 1,
+      question: '¿Qué es este sitio web?',
+      answer:
+        'Este sitio web es una plataforma para compartir conocimientos y obtener respuestas a preguntas comunes.',
+    },
+    {
+      id: 2,
+      question: '¿Cómo puedo crear una cuenta?',
+      answer:
+        "Para crear una cuenta, haz clic en el botón 'Registrarse' en la parte superior derecha y completa el formulario de registro.",
+    },
+    {
+      id: 3,
+      question: '¿Es gratis usar el sitio web?',
+      answer:
+        'Sí, el uso básico del sitio web es gratuito. Sin embargo, ofrecemos funciones premium que requieren una suscripción.',
+    },
+    {
+      id: 4,
+      question: '¿Cómo puedo restablecer mi contraseña?',
+      answer:
+        "Para restablecer tu contraseña, haz clic en '¿Olvidaste tu contraseña?' en la página de inicio de sesión y sigue las instrucciones.",
+    },
+  ];
 }
+
