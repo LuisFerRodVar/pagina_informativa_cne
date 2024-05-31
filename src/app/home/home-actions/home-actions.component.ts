03f5f77cb3c59a2ec4d0055419945a7afd69e286
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-05-31 11:42:03 -0600

    Added carrusel responsive

--- src/app/home/home-actions/home-actions.component.ts
+++ src/app/home/home-actions/home-actions.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, HostListener } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home-actions',
@@ -9,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeActionsComponent implements OnInit {
   currentIndex: number = 0;
+  itemsPerPage: number = 3;
   images: { id: string, src: string }[] = [
     { id: 'img1', src: '../../../assets/img/home/ComoNosPreparamos.webp' },
     { id: 'img2', src: '../../../assets/img/home/ComoActuar.webp' },
@@ -18,25 +20,59 @@ export class HomeActionsComponent implements OnInit {
     { id: 'img6', src: '../../../assets/img/home/ComoActuarInundacion.webp' }
   ];
 
-  ngOnInit(): void {}
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
 
-  getTransform(): string {
-    return `translateX(-${this.currentIndex * 100 / 3}%)`;
-  }
-
-  nextSlide(): void {
-    if (this.currentIndex + 3 < this.images.length) {
-      this.currentIndex += 3;
-    } else {
-      this.currentIndex = 0;
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateItemsPerPage();
     }
   }
 
-  prevSlide(): void {
-    if (this.currentIndex - 3 >= 0) {
-      this.currentIndex -= 3;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateItemsPerPage();
+    }
+  }
+
+  updateItemsPerPage(): void {
+    const width = window.innerWidth;
+    if (width >= 1024) {
+      this.itemsPerPage = 3;
+    } else if (width >= 768) {
+      this.itemsPerPage = 2;
     } else {
-      this.currentIndex = this.images.length - (this.images.length % 3 || 3);
+      this.itemsPerPage = 1;
+    }
+    this.updateTransform();
+  }
+
+  getTransform(): string {
+    return `translateX(-${this.currentIndex * 100 / this.itemsPerPage}%)`;
+  }
+
+  nextSlide(): void {
+    if (this.currentIndex + this.itemsPerPage < this.images.length) {
+      this.currentIndex += this.itemsPerPage;
+    } else {
+      this.currentIndex = 0;
+    }
+    this.updateTransform();
+  }
+
+  prevSlide(): void {
+    if (this.currentIndex - this.itemsPerPage >= 0) {
+      this.currentIndex -= this.itemsPerPage;
+    } else {
+      this.currentIndex = Math.max(0, this.images.length - this.itemsPerPage);
+    }
+    this.updateTransform();
+  }
+
+  updateTransform(): void {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    if (carousel) {
+      carousel.style.transform = this.getTransform();
     }
   }
 }
