9fe2278b2bb7d5062b2ae945273f86cae83fce95
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-05-31 11:23:04 -0600

    Added funcionality carrousel

--- src/app/home/home-actions/home-actions.component.ts
+++ src/app/home/home-actions/home-actions.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-actions',
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './home-actions.component.html',
   styleUrl: './home-actions.component.css'
 })
-export class HomeActionsComponent {
+export class HomeActionsComponent implements OnInit {
+  currentIndex: number = 0;
+  images: { id: string, src: string }[] = [
+    { id: 'img1', src: '../../../assets/img/home/ComoNosPreparamos.webp' },
+    { id: 'img2', src: '../../../assets/img/home/ComoActuar.webp' },
+    { id: 'img3', src: '../../../assets/img/home/ComoActuarInundacion.webp' },
+    { id: 'img4', src: '../../../assets/img/home/ComoNosPreparamos.webp' },
+    { id: 'img5', src: '../../../assets/img/home/ComoActuar.webp' },
+    { id: 'img6', src: '../../../assets/img/home/ComoActuarInundacion.webp' }
+  ];
 
+  ngOnInit(): void {}
+
+  getTransform(): string {
+    return `translateX(-${this.currentIndex * 100 / 3}%)`;
+  }
+
+  nextSlide(): void {
+    if (this.currentIndex + 3 < this.images.length) {
+      this.currentIndex += 3;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  prevSlide(): void {
+    if (this.currentIndex - 3 >= 0) {
+      this.currentIndex -= 3;
+    } else {
+      this.currentIndex = this.images.length - (this.images.length % 3 || 3);
+    }
+  }
 }
