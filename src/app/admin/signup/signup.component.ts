7f7df55996e2226cc436ae3cf54b95058bb2a957
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-06-03 01:24:27 -0600

    feat: Add responsive login form with glassmorphism effect
    - Implemented responsive design for the login form.
    - Added glassmorphism effect to the login form with backdrop blur and transparency.
    - Ensured the form adjusts correctly for different screen sizes, including mobile devices.
    - Centered the form content for better alignment.
    - Maintained the original background color (#5addf0) with a translucent effect.
    - Updated the CSS for better alignment, padding, and font sizes.
    - Verified the functionality of the form with Angular Reactive Forms.

--- src/app/admin/signup/signup.component.ts
+++ src/app/admin/signup/signup.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      recordar: [false]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Formulario Enviado', this.loginForm.value);
+      // Aquí puedes manejar el envío del formulario, como enviarlo a un servidor
+    }
+  }
 }
