eea32e703b0a87626e0653d0f82b4bc623e34fd8
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-06-03 09:01:06 -0600

    Added aside html and basic css

--- src/app/admin/aside/aside.component.ts
+++ src/app/admin/aside/aside.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
+import { faBars, faBook, faGear, faHome, faNewspaper, faSchool, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.css'
 })
 export class AsideComponent {
+  faBars = faBars;
+  faHome = faHome;
+  faUser = faUser;
+  faNews = faNewspaper;
+  faBuilding = faBuilding;
+  faSchool = faSchool;
+  faBook = faBook;
+  faGear = faGear;
 
 }
