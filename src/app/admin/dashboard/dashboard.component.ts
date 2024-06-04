5a214039de0458eda62b27f9f00fca41f4c306b0
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-06-04 06:38:19 -0600

    Added dashboard component desing

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface DashboardItem {
+  id: number;
+  nombre: string;
+  correo: string;
+  contacto: string;
+  telefono: string;
+}
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -8,5 +15,17 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  usersCount: number = 100;
+  hostelsCount: number = 100;
+  committeesCount: number = 100;
 
+  dashboardItems: DashboardItem[] = [
+    { id: 1, nombre: 'Nombre 1', correo: 'correo1@example.com', contacto: 'Contacto 1', telefono: '123456789' },
+    { id: 2, nombre: 'Nombre 2', correo: 'correo2@example.com', contacto: 'Contacto 2', telefono: '987654321' },
+    // Agrega más elementos según sea necesario
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
