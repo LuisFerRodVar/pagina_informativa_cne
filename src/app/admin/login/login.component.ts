61c6fd8934bc8033b9e634332fbafcf8a50d47de
JustinAMB <justinmb@outlook.es>
2024-06-05 21:16:08 -0600

    Add config firebase

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,30 +1,48 @@
-
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  AngularFireAuth,
+  AngularFireAuthModule,
+} from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [AngularFireAuthModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   registroForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private auth: AngularFireAuth) {}
 
   ngOnInit(): void {
     this.registroForm = this.fb.group({
-      nombre: ['', Validators.required],
       correo: ['', [Validators.required, Validators.email]],
-      contrasena: ['', [Validators.required, Validators.minLength(6)]]
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  onSubmit(): void {
+  async onSubmit() {
+  
     if (this.registroForm.valid) {
+  
       console.log('Formulario Enviado', this.registroForm.value);
       // Aquí puedes manejar el envío del formulario, como enviarlo a un servidor
+      const password = this.registroForm.value['contrasena'];
+      const email = this.registroForm.value['correo'];
+      console.log({email,password});
+      const result = await this.auth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+      const token=await result.user?.getIdToken()
+      console.log({result,token});
     }
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp } from 'firebase/app';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment.development';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideRouter(routes),
+    provideClientHydration(),
+    
+  ],
 };
