afaa96485e2682130b4af56f041f48bb6df10bda
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-06-11 02:41:49 -0600

    Added CRUD funcionality for Committees components and directories components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,12 +5,16 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { UsersComponent } from './users/users.component';
 import { NewsComponent } from './news/news.component';
 import { HostelsComponent } from './hostels/hostels.component';
+import { DirectoriesComponent } from './directories/directories.component';
+import { CommitteesComponent } from './committees/committees.component';
 
 @NgModule({
   declarations: [
     UsersComponent,
     NewsComponent,
     HostelsComponent,
+    DirectoriesComponent,
+    CommitteesComponent
   ],
   imports: [
     CommonModule,
