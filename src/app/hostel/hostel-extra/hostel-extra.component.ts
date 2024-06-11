0a2dd08870a5896698c5bf624ac1759b9478622f
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-06-10 18:33:08 -0600

    Basic styles changes

--- src/app/hostel/hostel-extra/hostel-extra.component.ts
+++ src/app/hostel/hostel-extra/hostel-extra.component.ts
@@ -8,28 +8,111 @@ import { Component } from '@angular/core';
   styleUrl: './hostel-extra.component.css'
 })
 export class HostelExtraComponent {
-  hostels = [{
-    district: 'Guaycara',
-    community: 'Río Claro',
-    type: 'Escuela',
-    capacity: 40,
-    inCharge: 'Paco',
-    contact: '8888-8888'
-  },
-  {
-    district: 'Guaycara',
-    community: 'Río Claro',
-    type: 'Escuela',
-    capacity: 40,
-    inCharge: 'Paco',
-    contact: '8888-8888'
-  },
-  {
-    district: 'Guaycara',
-    community: 'Río Claro',
-    type: 'Escuela',
-    capacity: 40,
-    inCharge: 'Paco',
-    contact: '8888-8888'
-  }]
+  hostels = [
+    {
+      district: 'Golfito',
+      community: 'Golfito Centro',
+      type: 'Escuela',
+      capacity: 0,
+      inCharge: 'Sergio Pérez Aymerich',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Golfito',
+      community: 'La Mona',
+      type: 'Escuela',
+      capacity: 0,
+      inCharge: 'Heber Cerdas Fuentes',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'Río Claro',
+      type: 'Salón comunal',
+      capacity: 0,
+      inCharge: 'Cecilia Femell Chavarría',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'Río Claro',
+      type: 'Escuela',
+      capacity: 0,
+      inCharge: 'Nancy Campos Porras',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'Coto 56 y 57',
+      type: 'Escuela',
+      capacity: 0,
+      inCharge: 'Marisol Esquivel Chichilla',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'San Ramón',
+      type: 'Escuela',
+      capacity: 0,
+      inCharge: 'Sidey Padilla Pérez',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'La Julieta',
+      type: 'Salón comunal',
+      capacity: 0,
+      inCharge: 'José Rosales Carballo',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'Rio Claro',
+      type: 'Aulas INA',
+      capacity: 0,
+      inCharge: 'Carlos Murillo Vargas',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'Rio Claro',
+      type: 'Iglesia',
+      capacity: 0,
+      inCharge: 'Toribio Jiménez Baltodano',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Guaycara',
+      community: 'San Ramón',
+      type: 'Salón comunal',
+      capacity: 0,
+      inCharge: 'Carmen Velencia Molina',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Pavón',
+      community: 'Comte',
+      type: 'Escuela',
+      capacity: 0,
+      inCharge: 'Luis Baltodano Jiménez',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Pavón',
+      community: 'Zancudo',
+      type: 'Escuela',
+      capacity: 0,
+      inCharge: 'Jessica Días Baltodano',
+      contact: 'no disponible'
+    },
+    {
+      district: 'Pavón',
+      community: 'Comte',
+      type: 'Gimnasio Liceo',
+      capacity: 0,
+      inCharge: 'Alfonso Rojas Loría',
+      contact: 'no disponible'
+    },
+
+  ]
 }
