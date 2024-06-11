71fbe91717c5f687837c40bb022fd3f8390c8534
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-06-11 01:31:37 -0600

    fix bug 2 jajaja

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule  } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
 import { UsersComponent } from './users/users.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { NewsComponent } from './news/news.component';
 import { HostelsComponent } from './hostels/hostels.component';
 
